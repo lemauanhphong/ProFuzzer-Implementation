a535212ca8f4c8f29da80f0a1298899463fc06ab
Le Mau Anh Phong <lemauanhphong@gmail.com>
2023-12-18 23:19:13 +0700

    Add probe to exploration mutation

--- fuzz/fuzz.sh
+++ fuzz/fuzz.sh
@@ -1,7 +1,10 @@
 export AFL_CUSTOM_MUTATOR_ONLY=1
 export AFL_CUSTOM_MUTATOR_LIBRARY="/home/paml/doanvuive/ProFuzzer-Implementation/mutator/exploration.so"
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
-g++ -shared -Wall -O3 -I ../mutator/include ../mutator/exploration.cpp ../probe/probe.cpp -fPIC -o ../mutator/exploration.so -lstdc++fs
+export AFL_DISABLE_TRIM=1
+export IGNORE_EXE_STDOUT=1
+export TARGET="../target/vuln"
+g++ -shared -Wall -O3 -fPIC -I../mutator/include ../mutator/exploration.cpp ../probe/probe.cpp  -o ../mutator/exploration.so -lstdc++fs &&
 # g++ -shared -Wall -O3 ../mutator/exploitation.cpp -o ../mutator/exploitation.so
 # gcc -shared -Wall -O3 ../mutator/exploration.c -o ../mutator/exploration.so
 # gcc -shared -Wall -O3 ../mutator/exploitation.c -o ../mutator/exploitation.so
